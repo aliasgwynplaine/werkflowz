d546870801a549b34d04e05751d44e18ee091dc1
Marco LEON <leon@newmail.lip6.fr>
2026-05-19 21:22:10 +0200

    i forgot the launcher lol

--- scripts/run_launcher.sh
+++ scripts/run_launcher.sh
@@ -4,24 +4,45 @@
 
 set -e
 
-if [ $# -lt 4 ]; then
-	echo "usage: $0 <func_id> <fprocess_mode> <fprocess>" 
+if [ $# -lt 2 ]; then
+	echo "usage: $0 <experiment-name> <func_id> <gateway_addr>" 
 	exit 1
 fi
 
-source common.env # fix this
+lhost=$(hostname)
+
+if [ ! -d "$HOME/$1" ]; then
+	echo "[$lhost]: Experiment not loaded"
+	exit 42
+fi
+
+experiment=$1
+cd $HOME/$experiment
+bash compil.sh
+source common.env
 
 if [ -z "$COMMON_ENV_H" ]; then
     echo '$COMMON_ENV_H is not defined'
     exit 2
 fi
 
-func_id=$1
-fprocess_mode=$2
-fprocess=$3
+func_id=$2
 
-$NIGHTCORE_ROOT/bin/$BUILD_TYPE/launcher \
+# all this info will be retrieved from the nighcore-config...
+fprocess_mode=$(cat nightcore_config.json | jq -r ".fprocess_mode")
+fprocess=$(cat nightcore_config.json | jq -r ".fuxmap[$((func_id - 1))]")
+
+if [ "$fprocess" = "null" ]; then
+	echo "[$lhost]: error bad func_id"
+	exit 43
+fi
+
+export NIGHTCORE_GW_ADDR=$3
+
+$NIGHTCORE_ROOT/bin/release/launcher \
     --func_id=$func_id --fprocess_mode=$fprocess_mode \
     --fprocess_output_dir=$BASE_DIR/outputs \
     --fprocess=$BASE_DIR/$fprocess \
     --v=1 2>$BASE_DIR/outputs/launcher_$fprocess.log &
+
+echo "[$lhost]: $fprocess_mode function $func_id - $fprocess  has been launched."
