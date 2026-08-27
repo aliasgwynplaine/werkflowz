249ad8f3c8ae6754fc399fc1227a5fb0a28699bd
Marco LEON <leon@newmail.lip6.fr>
2026-08-27 23:34:30 +0200

    update data gen script

--- scripts/get_my_fockin_data_c.sh
+++ scripts/get_my_fockin_data_c.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 set -e
 
-if [ $# -ne 1 ]; then
-	echo "usage: $0 <job-id>"
+if [ $# -ne 2 ]; then
+	echo "usage: $0 <job-id> <micro-bench>"
 	exit 1
 fi
 
 jobid=$1
+mb=$2
+
+case "$mb" in
+	micro-ccmesh)
+		runner="run_micro_ccmesh_launchers.sh"
+		;;
+	micro-ccmesg)
+		runner="run_micro_ccmesg_launchers.sh"
+		;;
+	micro-ccmesgbox)
+		runner="run_micro_ccmesgbox_launchers.sh"
+		;;
+	*)
+		echo "Unknown micro-bench: $mb"
+		exit 1
+		;;
+esac
 
 mapfile -t redishost < "redis.${jobid}"
 mapfile -t schedhost < "scheduler.${jobid}"
@@ -17,23 +34,28 @@ rpss="50 100 150 200 225 250 275 300 325 350 400 425 450 475 500 1000 1500 2000 
 
 if [ ! -d "data" ]; then
 	mkdir data
+	mkdir -p data/wrk
+	mkdir -p data/mon
 fi
 
+echo $rps > data/.metadata
+echo $mb >> data/.metadata
+
 for rps in $rpss; do
-	echo "[*] Running ${rps} req per sec"
+	echo "[*] Running ${rps} req/s on $runner"
 	echo "[*] Flushing all data"
 	bash remote_exec_sync.sh $redishost flushall.sh
 	wait
 
-	bash run_micro_ccmesh_launchers.sh $jobid
+	bash $runner $jobid
 	wait
 	
 	bash remote_exec_sync.sh $schedhost run_wrk2.sh $rps
 	wait
 
-	scp root@${workers[0]}:~/ccmesh-server/monitor.txt data/${rps}_monitor.txt
+	scp root@${workers[0]}:~/ccmesh-server/monitor.txt data/mon/${rps}_monitor.txt
 done
 
-scp root@$schedhost:~/data/* data/
+scp root@$schedhost:~/data/* data/wrk/
 
 echo "Done!"
