da43cd3e0e8a7483b81cd788108ba42b4cb87ba2
gwynplaine <mnleonr@uni.pe>
2026-08-28 18:57:34 +0200

    update client ccmesgbox ccmesh

--- flowerkz/micro-ccmesg-box/compil.sh
+++ flowerkz/micro-ccmesg-box/compil.sh
@@ -5,6 +5,9 @@ echo "Building..."
 
 go build -o Entry1 src/main.go
 cp Entry1 Entry2
+cp Entry1 Entry3
+cp Entry1 Entry4
+cp Entry1 Entry5
 cp Entry1 Sink
 
 echo "Done!"
--- flowerkz/micro-ccmesh/compil.sh
+++ flowerkz/micro-ccmesh/compil.sh
@@ -5,5 +5,8 @@ echo "Building..."
 
 go build -o Entry1 src/main.go
 cp Entry1 Entry2
+cp Entry1 Entry3
+cp Entry1 Entry4
+cp Entry1 Entry5
 
 echo "Done!"
